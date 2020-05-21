e926212ba04385e8de3e2de2ba283c4c2399e555
leechiho <lch5482@gmail.com>
2020-05-21 17:59:52 +0900

    ejs

--- server.js
+++ server.js
@@ -2,16 +2,26 @@ var express = require('express')
 var path = require('path')
 var bodyParser = require('body-parser')
 var app = express()
-
+var apiRouter = require('./routes/apiRouter')
 
 
 app.set('views', path.join(__dirname+'/views'))
+
 app.set('view engine','ejs')
-// app.use()
 
+//bodyparser를 이용해 json파일 처리
+app.use(bodyParser.json())
+//인코딩 잘하게 해줌
+app.use(bodyParser.urlencoded({extended:false}))
 
+app.use('/',apiRouter)
+// app.use('/',(req, res)=>{
+
+// }))
+
+//get방식으로 라우팅처리 
 app.get('/', function(request,response){
-    console.log(request)
+    console.log(request.body)
     response.render('index')
 })
 
